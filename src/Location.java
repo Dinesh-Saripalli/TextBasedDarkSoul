83665b9ac3c592ef836decc671fa509aafb39348
kevinriek <70723712+kevinriek@users.noreply.github.com>
2020-09-13 21:16:53 -0500

    Add files via upload

---
+++ src/Location.java
@@ -0,0 +1,27 @@
+public class Location {
+    protected String name;              //Name of Location
+    protected String description;       //Description of Location
+    protected String investigation;     //What a 'closer look' reveals
+    protected String item;              //item contained in location  (change to array for multiple items?)
+    protected boolean gameEnd = false;
+    //Universal methods
+    public static void pt(String word) {
+        System.out.println(word);
+    }
+
+    public Location(){
+
+    }
+
+    public Location (String Name, String desc, String invest, String Item){
+        name = Name;
+        description = desc;
+        investigation = invest;
+        item = Item;
+    }
+
+    //Default 'item used' method that is redefined in instanced classes
+    public void itemUsed(String item){
+        pt("It has no effect");
+    }
+}
